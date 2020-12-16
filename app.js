19215d914b17b0440df4340f8b5df88c263aee55
jungee <jungebah@gmail.com>
2020-12-15 18:31:20 -0800

    remove browser open

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ function iflixSearch() {
         try {
             const browser = await puppeteer.launch({
                 args: ['--no-sandbox', '--disable-setuid-sandbox']
-                , headless: false
             });
 
             const page = await browser.newPage();
